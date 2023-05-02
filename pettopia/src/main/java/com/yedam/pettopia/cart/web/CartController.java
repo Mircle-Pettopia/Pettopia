65702f9c78530eec1dfdeb3b595b24495beed8f2
5Solkun <kyj147852@gmail.com>
2023-05-02 17:09:47 +0900

    칼퇴가능???

--- pettopia/src/main/java/com/yedam/pettopia/cart/web/CartController.java
+++ pettopia/src/main/java/com/yedam/pettopia/cart/web/CartController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CartController {
 	@GetMapping("CartMain")
-	public String artMain(Model model) {
+	public String cartMain(Model model) {
 		return "cart/cart";
+	}
+	@GetMapping("OrderMain")
+	public String orderMain(Model model) {
+		return "cart/order";
 	}
 }
