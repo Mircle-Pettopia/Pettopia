c85064905f96c77e8a5cbfd975ec1114e0c3a8ac
JjungEeunAae <wjddmsdo77@naver.com>
2023-05-03 01:14:56 +0900

    20230503 eunae

--- pettopia/src/main/java/com/yedam/pettopia/user/web/UserController.java
+++ pettopia/src/main/java/com/yedam/pettopia/user/web/UserController.java
@@ -1,15 +1,9 @@
 package com.yedam.pettopia.user.web;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-import org.apache.catalina.connector.Response;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,7 +29,13 @@ public class UserController {
 	}
 	
 	@GetMapping("/login")
-    public String login(){
+    public String login(@RequestParam(value="error", required = false) String error,
+    					@RequestParam(value="exception", required = false) String exception,
+    					Model model){
+		/* 에러와 예외를 모델에 담아 view resolve */
+		model.addAttribute("error", error);
+		model.addAttribute("exception", exception);
+		
         return "login";
     };
     
