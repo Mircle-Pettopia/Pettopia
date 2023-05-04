987bbf969bc1ea3ff26d57ea1bd7e9ab7294e6f4
kmj <asdzxc@naver.com>
2023-05-04 14:22:45 +0900

    gongjisahang

--- pettopia/src/main/java/com/yedam/pettopia/notice/service/NoticeService.java
+++ pettopia/src/main/java/com/yedam/pettopia/notice/service/NoticeService.java
@@ -2,11 +2,15 @@ package com.yedam.pettopia.notice.service;
 
 import java.util.List;
 
+
 import com.yedam.pettopia.notice.NoticeVO;
 
 public interface NoticeService {
 	//조회
 	public List<NoticeVO> getNoticeList();
+	
+	//단건조회
+	public NoticeVO getNoticeDetail(NoticeVO noticeVO);
 	
 	//등록
 	public int insertNotice(NoticeVO noticeVO);
