7831f7bcbb291ea5d05466322e510b5f14b4939b
5Solkun <kyj147852@gmail.com>
2023-05-11 17:31:45 +0900

    야호~

--- pettopia/src/main/java/com/yedam/pettopia/cart/mapper/CartMapper.java
+++ pettopia/src/main/java/com/yedam/pettopia/cart/mapper/CartMapper.java
@@ -6,4 +6,7 @@ import com.yedam.pettopia.cart.service.vo.CartVO;
 
 public interface CartMapper {
 	public List<CartVO> getCart(String meId);
+	public int setAmount(String crtId,int cnt);
+	public int delCart(String crtId);
+	public int delCartDetail(String crtId);
 }
