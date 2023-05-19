854568737b891e70c95641e96aacb4d158ad0071
kmj <asdzxc@naver.com>
2023-05-19 17:28:40 +0900

    커밋

--- pettopia/src/main/java/com/yedam/pettopia/review/mapper/ReviewMapper.java
+++ pettopia/src/main/java/com/yedam/pettopia/review/mapper/ReviewMapper.java
@@ -9,7 +9,7 @@ public interface ReviewMapper {
 
 	// 후기 작성 가능한 리스트
 	public List<ReviewVO> selectReviewList(String meId);
-
+	
 	// 후기 등록
 	public int insertReview(ReviewVO reviewVO);
 
@@ -21,5 +21,8 @@ public interface ReviewMapper {
 	
 	//후기 삭제
 	public void deleteReview(int reNo);
+	
+	//작성 후기 디테일
+	public ReviewVO writtenDetail(ReviewVO reviewVO);
 
 }
